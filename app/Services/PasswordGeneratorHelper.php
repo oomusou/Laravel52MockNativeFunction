9d5f993e8499b6738af35056ed94139efa141c1d
oomusou <oomusou@gmail.com>
2016-03-15 09:20:13 +0800

    新增PasswordGeneratorHelper.php，對str_shuffle()封裝

---
+++ app/Services/PasswordGeneratorHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Servives;
+
+class PasswordGeneratorHelper
+{
+    /**
+     * 將原生的str_shuffule()封裝
+     *
+     * @param string $origin
+     * @return string
+     */
+    public function str_shuffle(string $origin) : string
+    {
+        return str_shuffle($origin);
+    }
+}
+
