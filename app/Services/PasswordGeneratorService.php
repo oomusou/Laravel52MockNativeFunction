e77b8c0fdb4d92d4d96eff14f7c0e8647857d58d
oomusou <oomusou@gmail.com>
2016-03-15 09:26:49 +0800

    改使用PasswordGeneratorHelper的str_shuffle()

--- app/Services/PasswordGeneratorService.php
+++ app/Services/PasswordGeneratorService.php
@@ -4,6 +4,18 @@ namespace App\Servives;
 
 class PasswordGeneratorService
 {
+    /** @var PasswordGeneratorHelper */
+    private $passwordGeneratorHelper;
+
+    /**
+     * PasswordGeneratorService constructor.
+     * @param PasswordGeneratorHelper $passwordGeneratorHelper
+     */
+    public function __construct(PasswordGeneratorHelper $passwordGeneratorHelper)
+    {
+        $this->passwordGeneratorHelper = $passwordGeneratorHelper;
+    }
+
     /**
      * 產生密碼
      *
@@ -13,7 +25,7 @@ class PasswordGeneratorService
      */
     public function generate(string $origin, int $length) : string
     {
-        $target = str_shuffle($origin);
+        $target = $this->passwordGeneratorHelper->str_shuffle($origin);
         return substr($target, 0, $length);
     }
 }
